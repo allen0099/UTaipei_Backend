c0f1de7b3ac5192aaa843a00a70d6c01e947ebcc
秉虎 <s96016641@gmail.com>
2021-07-21 01:42:04 +0800

    add schedule table show and counts

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, redirect, render_template, session, url_for
 
 from functions import get_semester, get_year
-from models import Classes
+from models import Classes, Timetable
 
 web_routes: Blueprint = Blueprint("routes", __name__)
 
@@ -27,4 +27,23 @@ def query_result():
 
 @web_routes.route('/my_table')
 def my_table():
-    return render_template('schedule.html', year=get_year(), semester=get_semester())
+    times: list[Timetable] = []
+    classes: list[Classes] = []
+
+    if session.get("query"):
+        for _ in session.get("query"):
+            _q: list[Timetable] = Timetable.query.filter_by(class_id=_).all()
+            for t in _q:
+                times.append(t)
+            _c: Classes = Classes.query.filter_by(id=_).first()
+            classes.append(_c)
+
+    __mapper__: dict = {
+        "一": 1, "二": 2, "三": 3, "四": 4, "五": 5, "六": 6, "日": 7,
+    }
+    # https://stackoverflow.com/a/17109098
+    times.sort(key=lambda x: (int(x.time), __mapper__[x.weekday]))
+
+    return render_template('schedule.html', times=times, classes=classes,
+                           year=get_year(), semester=get_semester(), d_map=__mapper__,
+                           d_time=list(dict.fromkeys([_.time for _ in times])))
